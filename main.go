ce24f32cfe630b9dafeee466bbccafcc4c005583
Fakhri Bashiri <fakhribashiri@Fakhris-MacBook-Pro.local>
2024-02-19 15:29:45 +0700

    index category

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"golang-crud/config"
+	"golang-crud/controllers/categorycontroller"
 	"golang-crud/controllers/homecontroller"
 	"log"
 	"net/http"
@@ -12,6 +13,12 @@ func main() {
 
 	//1. Homepage
 	http.HandleFunc("/", homecontroller.Welcome)
+
+	//1. Categories
+	http.HandleFunc("/categories", categorycontroller.Index)
+	http.HandleFunc("/categories/add", categorycontroller.Add)
+	http.HandleFunc("/categories/edit", categorycontroller.Edit)
+	http.HandleFunc("/categories/delete", categorycontroller.Delete)
 
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
