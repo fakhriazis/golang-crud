be9d2ee9272e0766728b2982f0f990d729a2d9eb
Fakhri Bashiri <fakhribashiri@Fakhris-MacBook-Pro.local>
2024-02-19 21:53:57 +0700

    add category

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -1,9 +1,11 @@
 package categorycontroller
 
 import (
+	"golang-crud/entities"
 	categorymodel "golang-crud/models/categorymodel"
 	"net/http"
 	"text/template"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +23,29 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func Add(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/category/create.html")
+		if err != nil {
+			panic(err)
+		}
+
+		temp.Execute(w, nil)
+	}
+
+	if r.Method == "POST" {
+		var category entities.Category
+
+		category.Name = r.FormValue("name")
+		category.CreatedAt = time.Now()
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Create(category); !ok {
+			temp, _ := template.ParseFiles("views/category/create.html")
+			temp.Execute(w, nil)
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
 
 }
 
