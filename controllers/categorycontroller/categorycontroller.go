c6da060c7e8edc1b411eecd3060ec58d175953fe
Fakhri Bashiri <fakhribashiri@Fakhris-MacBook-Pro.local>
2024-02-19 22:31:28 +0700

    edit category

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -4,6 +4,7 @@ import (
 	"golang-crud/entities"
 	categorymodel "golang-crud/models/categorymodel"
 	"net/http"
+	"strconv"
 	"text/template"
 	"time"
 )
@@ -50,7 +51,45 @@ func Add(w http.ResponseWriter, r *http.Request) {
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/category/edit.html")
+		if err != nil {
+			panic(err)
+		}
 
+		idString := r.URL.Query().Get("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+
+		category := categorymodel.Detail(id)
+		data := map[string]any{
+			"category": category,
+		}
+
+		temp.Execute(w, data)
+	}
+
+	if r.Method == "POST" {
+		var category entities.Category
+
+		idString := r.FormValue("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+
+		category.Name = r.FormValue("name")
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Update(id, category); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+			return
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
