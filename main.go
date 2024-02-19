3ffece5acc21320d4f2b6c6e6f1d396e3fb875b1
Fakhri Bashiri <fakhribashiri@Fakhris-MacBook-Pro.local>
2024-02-19 13:16:30 +0700

    config database & homepage

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"golang-crud/config"
+	"golang-crud/controllers/homecontroller"
+	"log"
+	"net/http"
+)
 
+func main() {
+	config.ConnectDB()
+
+	//1. Homepage
+	http.HandleFunc("/", homecontroller.Welcome)
+
+	log.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
