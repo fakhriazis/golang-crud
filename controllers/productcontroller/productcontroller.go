2bd880aae33b58b02e7afe7596363b87427f7436
Fakhri Bashiri <fakhribashiri@Fakhris-MacBook-Pro.local>
2024-02-25 11:00:16 +0700

    add product 'post'

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -1,9 +1,13 @@
 package productcontroller
 
 import (
+	"golang-crud/entities"
+	categorymodel "golang-crud/models/categorymodel"
 	"golang-crud/models/productmodel"
 	"html/template"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -25,7 +29,47 @@ func Detail(w http.ResponseWriter, r *http.Request) {
 }
 
 func Add(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/product/create.html")
+		if err != nil {
+			panic(err)
+		}
 
+		categories := categorymodel.GetAll()
+		data := map[string]any{
+			"categories": categories,
+		}
+
+		temp.Execute(w, data)
+	}
+
+	if r.Method == "POST" {
+		var product entities.Product
+
+		categoryId, err := strconv.Atoi(r.FormValue("category_id"))
+		if err != nil {
+			panic(err)
+		}
+
+		stock, err := strconv.Atoi(r.FormValue("stock"))
+		if err != nil {
+			panic(err)
+		}
+
+		product.Name = r.FormValue("name")
+		product.Category.Id = uint(categoryId)
+		product.Stock = int64(stock)
+		product.Description = r.FormValue("description")
+		product.CreatedAt = time.Now()
+		product.UpdatedAt = time.Now()
+
+		if ok := productmodel.Create(product); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusTemporaryRedirect)
+			return
+		}
+
+		http.Redirect(w, r, "/products", http.StatusSeeOther)
+	}
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
