2813e4cdb46b91288fda3f41ba70bf31a1e2b827
Fakhri Bashiri <fakhribashiri@Fakhris-MacBook-Pro.local>
2024-02-25 09:26:56 +0700

    add products

---
+++ controllers/productcontroller/productcontroller.go
@@ -0,0 +1,37 @@
+package productcontroller
+
+import (
+	"golang-crud/models/productmodel"
+	"html/template"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	products := productmodel.GetAll()
+	data := map[string]any{
+		"products": products,
+	}
+
+	temp, err := template.ParseFiles("views/product/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	temp.Execute(w, data)
+}
+
+func Detail(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"golang-crud/config"
 	"golang-crud/controllers/categorycontroller"
 	"golang-crud/controllers/homecontroller"
+	"golang-crud/controllers/productcontroller"
 	"log"
 	"net/http"
 )
@@ -14,11 +15,18 @@ func main() {
 	//1. Homepage
 	http.HandleFunc("/", homecontroller.Welcome)
 
-	//1. Categories
+	//2. Categories
 	http.HandleFunc("/categories", categorycontroller.Index)
 	http.HandleFunc("/categories/add", categorycontroller.Add)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	//Product
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
